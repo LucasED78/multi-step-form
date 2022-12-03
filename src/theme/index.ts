e5fc372df9d6f53e55c11c99fbf4ffc1afbf854b
Lucas Eduardo <lucased78@hotmail.com>
2022-12-03 14:23:55 -0300

    feat: adding theme

---
+++ src/theme/index.ts
@@ -0,0 +1,32 @@
+export const theme = {
+  colors: {
+    marineBlue: 'hsl(213, 96%, 18%)',
+    purplishBlue: 'hsl(243, 100%, 62%)',
+    pastelBlue: 'hsl(228, 100%, 84%)',
+    lightBlue: 'hsl(206, 94%, 87%)',
+    strawberryRed: 'hsl(354, 84%, 57%)',
+    coolGray: 'hsl(231, 11%, 63%)',
+    lightGray: 'hsl(229, 24%, 87%)',
+    magnolia: 'hsl(217, 100%, 97%)',
+    alabaster: 'hsl(231, 100%, 99%)',
+    white: 'hsl(0, 0%, 100%)',
+  },
+  fontSize: {
+    XL: '32px',
+    XXL: '40px',
+    LG: '22px',
+    MD: '18px',
+    SM: '16px',
+    XS: '14px',
+    XXS: '12px',
+  },
+  lineHeight: {
+    XL: '190%',
+    LG: '170%',
+    MD: '150%',
+    SM: '130%',
+    XS: '110%',
+  },
+};
+
+export type Theme = typeof theme;
