1f854754df9940c148f36215541f41bd1b52726a
Lucas Eduardo <lucased78@hotmail.com>
2022-12-05 19:13:10 -0300

    feat: adding PersonalInfoStepForm component

---
+++ src/components/form/components/personal-info-step-form/index.tsx
@@ -0,0 +1,77 @@
+import { useFormikContext } from 'formik';
+import { useMemo } from 'react';
+import { useStepperContext } from '../../../../context/stepper-context';
+import { ESubscriptionSteps } from '../../../../types';
+import { ISubscriptionForm } from '../../../../typing';
+import Button from '../../../button';
+import Input from '../../../input';
+import FormWrapper from '../form-wrapper';
+import * as css from '../../styles';
+
+const PersonalInfoStepForm = () => {
+  const { values, errors, touched, handleChange, handleBlur } =
+    useFormikContext<ISubscriptionForm>();
+  const { setStep } = useStepperContext();
+  const keys = ['name', 'emailAddress', 'phoneNumber'];
+
+  const isValid = useMemo(() => {
+    return !keys.some(
+      key =>
+        (touched[key as keyof ISubscriptionForm] &&
+          !!errors[key as keyof ISubscriptionForm]) ??
+        !touched[key as keyof ISubscriptionForm],
+    );
+  }, [touched, errors]);
+
+  return (
+    <FormWrapper
+      title="Personal info"
+      subtitle="Please provide your name, email address, and phone number"
+    >
+      <css.FormContentContainer>
+        <css.InputGroup>
+          <Input
+            label="Name"
+            name="name"
+            placeholder="Provide your name"
+            value={values.name}
+            hasError={touched.name && !!errors.name}
+            helperText={errors.name}
+            onChange={handleChange}
+            onBlur={handleBlur}
+          />
+          <Input
+            label="Email Address"
+            name="emailAddress"
+            placeholder="Your best email address"
+            value={values.emailAddress}
+            hasError={touched.emailAddress && !!errors.emailAddress}
+            helperText={errors.emailAddress}
+            onChange={handleChange}
+            onBlur={handleBlur}
+          />
+          <Input
+            label="Phone Number"
+            name="phoneNumber"
+            placeholder="e.g. +1 234 567 890"
+            value={values.phoneNumber}
+            hasError={touched.phoneNumber && !!errors.phoneNumber}
+            helperText={errors.phoneNumber}
+            onChange={handleChange}
+            onBlur={handleBlur}
+          />
+        </css.InputGroup>
+        <css.ButtonContainer>
+          <Button
+            onClick={() => setStep(ESubscriptionSteps.PlanSelector)}
+            disabled={!isValid}
+          >
+            Next step
+          </Button>
+        </css.ButtonContainer>
+      </css.FormContentContainer>
+    </FormWrapper>
+  );
+};
+
+export default PersonalInfoStepForm;
