168de6194a0d4f73575c61a6cae16adabab9bd80
Lucas Eduardo <lucased78@hotmail.com>
2022-12-05 19:15:28 -0300

    feat: using Form on App component

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,42 @@
+import { useMediaQuery } from 'react-responsive';
+import { ThemeProvider } from 'styled-components';
+import StepperSidebar from './components/stepper-sidebar';
+import Wrapper from './components/wrapper';
+import { GlobalStyles } from './styles';
+import { theme } from './theme';
+import Form from './components/form';
+import withContext from './hocs/with-context';
+import {
+  StepperContextProvider,
+  useStepperContext,
+} from './context/stepper-context';
+import { PlanContextProvider } from './context/plan-context';
+
 const App = () => {
-  return <>app</>;
+  const isMobile = useMediaQuery({
+    query: '(max-width: 768px)',
+  });
+  const { step } = useStepperContext();
+
+  return (
+    <ThemeProvider theme={theme}>
+      <GlobalStyles />
+
+      <Wrapper
+        sidebarComponent={
+          <StepperSidebar
+            step={step}
+            steps={['Your info', 'Select plan', 'Add-ons', 'Summary']}
+            variant={isMobile ? 'horizontal' : 'vertical'}
+          />
+        }
+      >
+        <PlanContextProvider>
+          <Form />
+        </PlanContextProvider>
+      </Wrapper>
+    </ThemeProvider>
+  );
 };
 
-export default App;
+export default withContext(StepperContextProvider, App);
